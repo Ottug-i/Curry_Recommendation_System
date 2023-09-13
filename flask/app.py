19f9c58a62a2b78102dd0d503e5709a0a6db4d10
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-09-13 15:12:25 +0900

    Fix csv to db

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,11 @@
 from flask import Flask, json, request
 from py.bookmark_recommend import recommend_with_bookmark
-from py.rating_recommend import find_user_ratings
-from py.rating_recommend import update_or_append_user_ratings
-from py.rating_recommend import delete_user_rating
 from py.rating_recommend import find_favorite_genre_with_bookmark
 from py.rating_recommend import recommend_with_ratings
 
 app = Flask(__name__)
 
-@app.route("/hello", methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def hello():
     return "hello world"
 
@@ -24,51 +21,6 @@ def get_bookmark_recommend():
         result = recommend_with_bookmark(recipe_id, page)
     except KeyError as e:
         return json.dumps({"error": "Recipe ID not found"}), 404
-    
-    return json.dumps(result), 200
-
-@app.route('/rating/user_ratings', methods=['GET'])
-def get_user_ratings():
-    try:
-        user_id = int(request.args.get('user_id'))
-        recipe_id = int(request.args.get('recipe_id'))
-    except (TypeError, ValueError):
-        return json.dumps({"error": "Invalid parameters"}), 400
-
-    try:
-        result = find_user_ratings(user_id, recipe_id)
-    except KeyError as e:
-        return json.dumps({"error": "User rating not found for the given user_id and recipe_id."}), 404
-
-    return json.dumps(result), 200
-
-@app.route('/rating/user_ratings', methods=['POST'])
-def update_user_ratings():
-    try:
-        user_id = int(request.json.get('user_id'))
-        new_user_ratings_dic = request.json.get('new_user_ratings_dic')
-    except (TypeError, ValueError):
-        return json.dumps({"error": "Invalid parameters"}), 400
-
-    try:
-        result = update_or_append_user_ratings(user_id, new_user_ratings_dic)
-    except KeyError as e:
-        return json.dumps({"error": "User rating not found for the given user_id and recipe_id."}), 404
-    
-    return json.dumps(result), 200
-    
-@app.route('/rating/user_ratings', methods=['DELETE'])
-def remove_user_ratings():
-    try:
-        user_id = int(request.args.get('user_id'))
-        recipe_id = int(request.args.get('recipe_id'))
-    except (TypeError, ValueError):
-        return json.dumps({"error": "Invalid parameters"}), 400
-
-    try:
-        result = delete_user_rating(user_id, recipe_id)
-    except KeyError as e:
-        return json.dumps({"error": "User rating not found for the given user_id and recipe_id."}), 404
     
     return json.dumps(result), 200
 
--- flask/py/rating_recommend.py
+++ flask/py/rating_recommend.py
@@ -1,3 +1,4 @@
+import pymysql
 import pandas as pd
 import csv
 import os
@@ -20,109 +21,47 @@ def find_favorite_genre_with_bookmark(user_id, bookmark_list):
     favorite_genre = max(genre_count, key=genre_count.get)
     return user_id, favorite_genre
 
-def find_user_ratings(user_id, recipe_id):
-
-    curry_recipes = pd.read_csv('./csv/curry_recipe_with_genres.csv', index_col='id')
-
-    curry_ratings = pd.read_csv('./csv/user_ratings.csv', index_col = 'id')
-
-    user_ratings = curry_ratings[curry_ratings['userId'] == user_id]
-
-    if recipe_id in user_ratings.index:
-        return [recipe_id] + [user_id] + user_ratings.loc[recipe_id, ['rating']].tolist()
-    elif recipe_id not in user_ratings.index and recipe_id in curry_recipes.index:
-        return None
-    else:
-        return False
-
-def update_or_append_user_ratings(user_id, new_user_ratings_dic):
-
-    curry_recipes = pd.read_csv('./csv/curry_recipe_with_genres.csv', index_col='id')
-
-    file_path = './csv/user_ratings.csv'
-
-    file_exists = os.path.exists(file_path)
-
-    existing_ratings = []
-    if file_exists:
-        with open(file_path, 'r') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            existing_ratings = [row for row in csv_reader]
-
-        with open(file_path, 'w', newline='') as csvfile:
-            fieldnames = ['id', 'userId', 'rating']
-            csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            csv_writer.writeheader()
-
-            updated = False
-
-            for rating in existing_ratings:
-                if (rating['userId'] == str(user_id)) and (rating['id']) in new_user_ratings_dic:
-                    rating['rating'] = new_user_ratings_dic[rating['id']]
-                    updated = True
-
-                csv_writer.writerow(rating)   
-
-            for key, val in new_user_ratings_dic.items():
-                if (int(key) in curry_recipes.index) and not any(rating['userId'] == str(user_id) and rating['id'] == key for rating in existing_ratings):
-                    csv_writer.writerow({'id': key, 'userId': str(user_id), 'rating': val})
-                    updated = True
-            return updated
-        
-def delete_user_rating(user_id, recipe_id):
-    
-    file_path = './csv/user_ratings.csv'
-    file_exists = os.path.exists(file_path)
-    
-    if file_exists:
-        with open(file_path, 'r') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            existing_ratings = [row for row in csv_reader]
-        
-        filtered_ratings = [row for row in existing_ratings if row['userId'] != str(user_id) or row['id'] != str(recipe_id)]
-        
-        with open(file_path, 'w', newline='') as csvfile:
-            fieldnames = existing_ratings[0].keys()
-            csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            csv_writer.writeheader()
-            csv_writer.writerows(filtered_ratings)
-        
-        if len(existing_ratings) == len(filtered_ratings):
-            return False
-        else:
-            return True
-    return False
-
 def recommend_with_ratings(user_id, favorite_genre, page):
 
-    user_ratings = pd.read_csv('./csv/user_ratings.csv')
+    db = pymysql.connect(host='{MYSQL_URL}', port=3306, user='{MYSQL_USERNAME}', password='{MYSQL_PW}', db='curry', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    curry_genres = pd.read_csv('./csv/genres.csv')
+    try:
+        with db.cursor() as cursor:
+            sql = "SELECT * FROM curry.ratings"
+            cursor.execute(sql)
+            user_ratings = pd.DataFrame(cursor.fetchall())
+            print(user_ratings)
 
-    user_ratings = user_ratings.merge(curry_genres, left_on='id', right_on='id')
+            curry_genres = pd.read_csv('./csv/genres.csv')
 
-    user_profile = user_ratings[user_ratings['userId'] == user_id]
+            user_ratings = user_ratings.merge(curry_genres, left_on='recipe_id', right_on='id')
+            print(user_ratings)
 
-    X_train, X_test, y_train, y_test = train_test_split(user_profile[curry_genres.columns], user_profile['rating'], random_state=42, test_size=0.1)
+            user_profile = user_ratings[user_ratings['user_id'] == user_id]
 
-    lasso = Lasso(alpha=0.005)
+            X_train, X_test, y_train, y_test = train_test_split(user_profile[curry_genres.columns], user_profile['rating'], random_state=42, test_size=0.1)
 
-    lasso.fit(X_train, y_train)
+            lasso = Lasso(alpha=0.005)
 
-    predictions = lasso.predict(curry_genres)
-    curry_genres['predict'] = predictions
+            lasso.fit(X_train, y_train)
 
-    if favorite_genre is None:
-        sorted_user_predict = curry_genres.sort_values(by='predict', ascending=False)
-    else:
-        sorted_user_predict = curry_genres.sort_values(by=['predict', favorite_genre], ascending=[False, False])
+            predictions = lasso.predict(curry_genres)
+            curry_genres['predict'] = predictions
 
-    user_predict_ids = sorted_user_predict.id.tolist()
+            if favorite_genre is None:
+                sorted_user_predict = curry_genres.sort_values(by='predict', ascending=False)
+            else:
+                sorted_user_predict = curry_genres.sort_values(by=['predict', favorite_genre], ascending=[False, False])
 
-    start_idx = (page - 1) * 10
-    end_idx = start_idx + 10
+            user_predict_ids = sorted_user_predict.id.tolist()
 
-    if start_idx >= len(user_predict_ids):
-        return None
+            start_idx = (page - 1) * 10
+            end_idx = start_idx + 10
 
-    return user_predict_ids[start_idx:end_idx]
+            if start_idx >= len(user_predict_ids):
+                return None
+
+            return user_predict_ids[start_idx:end_idx]
+    
+    finally:
+        db.close()
